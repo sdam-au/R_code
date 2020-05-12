2e6f6df39cb5021a628a267bbdc3fcbe3d9d022d
Petra Janouchova <petra.janouchova@gmail.com>
2020-05-12 16:25:23 +0200

    trying to upload json back to sciencedata

--- EDH_to_tibble.R
+++ EDH_to_tibble.R
@@ -16,13 +16,20 @@ user <- readline("your sciencedata username: ")
 resp = request("EDH_utf8.json", path="/sharingin/648597@au.dk/SDAM_root/SDAM_data/EDH/", method="GET", cred=c(user, getPass("your sciencedata password: ")))
 # Make a list from the request
 list_json <- fromJSON(resp)
-# 
+# transform to tibble
 EDH_tibble = as_tibble(list_json)
 head(EDH_tibble)
 
-
+#subset the tibble
 Thracia <- EDH_tibble %>% 
-  filter(province_label=="Thracia")
+  filter(province_label=="Thracia"| province_label=="Thracia?")
 Thracia
 
-save(Thracia, file = "data/Thracia.RData")
+#transform the subset to json
+Thracia_json <- toJSON(Thracia)
+
+# save to Sciencedata
+request("Thracia_json", path="/sharingout/648597@au.dk/SDAM_root/SDAM_data/EDH/", method="PUT", cred=c(user, getPass("your sciencedata password: ")),
+        subdomain = "silo4") 
+
+
