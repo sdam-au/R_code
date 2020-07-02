b49f9fed81587054c19bcb615a4de7e5caf5847e
Petra Janouchova <petra.janouchova@gmail.com>
2020-07-02 14:30:36 +0200

    tested new improvement of the request function

--- EDH_to_tibble.R
+++ EDH_to_tibble.R
@@ -1,11 +1,8 @@
 ## Install packages
 devtools::install_github("mplex/cedhar", subdir="pkg/sdam")
-install.packages("rjson")
-install.packages("tidyverse")
-install.packages("getPass")
 ## Load packages
 library(sdam)
-library(rjson)
+library(jsonlite)
 library(tidyverse)
 library(getPass)
 
@@ -13,27 +10,27 @@ library(getPass)
 user <- readline("your sciencedata username: ")
 
 # Make the request (you will be asked for password in a new pop-up window)
-resp = request("EDH_utf8.json", path="/sharingin/648597@au.dk/SDAM_root/SDAM_data/EDH/", method="GET", cred=c(user, getPass("your sciencedata password: ")))
+resp = request("EDH_utf8_sample.json", path="/sharingin/648597@au.dk/SDAM_root/SDAM_data/EDH/", method="GET", cred=c(user, getPass("your sciencedata password: ")))
 
 # Make a list from the request
-list_json <- fromJSON(resp)
+list_json <- jsonlite::fromJSON(resp)
 
 # transform to tibble
 EDH_tibble = as_tibble(list_json)
 head(EDH_tibble)
 
-# subset the tibble only to inscriptions found in Thracia
-Thracia <- EDH_tibble %>% 
-  filter(province_label=="Thracia"| province_label=="Thracia?")
-Thracia
+# subset the tibble only to inscription_type containing word 'votive'
+votive_insc <- EDH_tibble %>% 
+  filter(type_of_inscription==(str_subset(type_of_inscription, "votive")))
+votive_insc
 
 # transform the subset to json and save it
-Thracia_json <- toJSON(Thracia)
-write(Thracia_json, file="data/EDH_Thracia.json")
+votive_insc_json <- toJSON(votive_insc)
+write(votive_insc_json, file="data/EDH_votive_sample.json")
 
 # save to Sciencedata into the sdam shared drive 
-request("data/EDH_Thracia.json", path="/sharingout/648597@au.dk/SDAM_root/SDAM_data/EDH/", 
-        method="PUT", cred=c(user, getPass("your sciencedata password: "))) 
+request("data/EDH_votive_sample.json", path="/sharingout/648597@au.dk/SDAM_root/SDAM_data/EDH/", 
+        method="PUT", force=TRUE, cred=c(user, getPass("your sciencedata password: "))) 
 
 # if you got Status 201 response in the console, the file should be now on Sciencedata.dk in the folder specified. 
 # If you got Status 401 or 403, there is problem with your authentication to Sciencedata.dk. You may check 
@@ -41,3 +38,4 @@ request("data/EDH_Thracia.json", path="/sharingout/648597@au.dk/SDAM_root/SDAM_d
 # b) rerun the user <- readline("your sciencedata username: ")
 # c) try with an array for credentials such as cred <- c("USERNAME", "PASSWORD")
 
+# request(file, method=PUT, force=TRUE)
